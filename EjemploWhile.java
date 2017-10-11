f1a6bd46a3fcc9428a7f53d5b4bb79e2c24b6a75
nico1994 <31898579+nico1994@users.noreply.github.com>
2017-10-11 16:37:17 -0300

    Add files via upload

--- EjemploWhile.java
+++ EjemploWhile.java
@@ -14,13 +14,12 @@ import java.util.Scanner;
 public class EjemploWhile {
 
     /**
-     * 
-     * @param args
+     * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-    //EjemploWhile.PromedioConIf();
-    EjemploWhile.PedirNumeros();
+        // TODO code application logic here
+         //EjemploWhile.PromedioConIf();
+    EjemploWhile.TomarDatosValidados();
     }//finaliza el main
     
     /**
@@ -137,6 +136,118 @@ public class EjemploWhile {
     }
     
     
- 
+    /**
+     * Pide los datos de una persona y valida cada uno
+     * datos a pedir:
+    nombre,edad(entre 1 y 100),sexo(f o m),estado civil(s c d v), nacionalidad
+    (N,E,L,EURO,R)
+    Sueldo (entre 8000 y 58000)
+    Donar organos (Si o No)
+    * clave de no menos de 6 caracteres
+   
+    */
+    public static void TomarDatosValidados()
+    {
+        Scanner MiTeclado= new Scanner(System.in);
+        String Aux;
+        String Nombre,Sexo,EstadoCivil,Nacionalidad,Donador,Clave;
+        Integer Edad,Sueldo;
+        
+        System.out.println("¿Cuál es su nombre?");
+        Nombre= MiTeclado.next();
+        
+        System.out.println("Indique su edad : ");
+        Aux= MiTeclado.next();
+        Edad= Integer.parseInt(Aux);
+        
+        while (Edad<1 || Edad>100)
+        {
+            System.out.println("Ingrese su edad nuevamente: ");
+            Aux= MiTeclado.next();
+            Edad= Integer.parseInt(Aux);}
+        
+        
+        System.out.println("Indique sexo : ");
+        Sexo= MiTeclado.next();
+            
+        while (!Sexo.equals("masculino") && !Sexo.equals("femenino") )
+         { System.out.println("Ingrese su sexo nuevamente: ");
+             Sexo= MiTeclado.next();}
+             
+           
+            System.out.println("Estado civil?");
+            EstadoCivil= MiTeclado.next();
+            
+        while (!EstadoCivil.equals("casado") && !EstadoCivil.equals("divorciado") &&
+                    !EstadoCivil.equals("soltero") && !EstadoCivil.equals("viudo"))
+            
+                { System.out.println("Ingrese su estado civil nuevamente: ");
+                  EstadoCivil= MiTeclado.next();}
+            
+            System.out.println("Nacionalidad? ");
+            Nacionalidad= MiTeclado.next();
+            
+        while (!Nacionalidad.equals("nativo")&& !Nacionalidad.equals("europeo")
+                  &&  !Nacionalidad.equals("extranjero") && !Nacionalidad.equals("resto del mundo")
+                  &&  !Nacionalidad.equals("latinoamericano"))
+                {
+                System.out.println("Ingrese su nacionalidad nuevamente: ");
+                Nacionalidad= MiTeclado.next();}
+            
+            System.out.println("Ingrese su sueldo: ");
+            Aux= MiTeclado.next();
+            Sueldo= Integer.parseInt(Aux);
+               
+        while (!(Sueldo>8000 && Sueldo<58000))
+               {
+                System.out.println("Ingrese su sueldo nuevamente :");
+                Aux= MiTeclado.next();
+                Sueldo= Integer.parseInt(Aux);}
+                   
+            System.out.println("¿Donar organos? ");
+            Donador= MiTeclado.next();
+               
+        while (!Donador.equals("si") && !Donador.equals("no"))
+               {
+                System.out.println("Ingrese nuevamente (si o no)");
+                Donador= MiTeclado.next();}
+              
+            System.out.println("Ingrese una clave de no menos de 6 caracteres:");
+            Clave= MiTeclado.next();
+               
+        while (!Clave.equals("123456"))
+               {
+                System.out.println("Ingrese clave nuevamente: ");
+                Clave=MiTeclado.next();}
+               
+              
+               
+               
+               
+               
+               
+               System.out.println("Su nombre es: "+Nombre);
+               System.out.println("Su edad es: "+Edad);
+               System.out.println("Su sexo es: "+Sexo);
+               System.out.println("Su Estado Civil es: "+EstadoCivil);
+               System.out.println("Su nacionalidad es: "+Nacionalidad);
+               System.out.println("Su sueldo es: "+Sueldo);   
+               System.out.println("Dona organos: "+Donador);
+               
+               }
+        
+        
+    }
     
-}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
